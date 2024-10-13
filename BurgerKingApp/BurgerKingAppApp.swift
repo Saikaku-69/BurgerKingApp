293639272c381a6bc8a872095ab4629e12d8e454
Saikaku-69 <grnmnsta@gmail.com>
2024-10-13 13:09:59 +0900

    update initial files

---
+++ BurgerKingApp/BurgerKingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BurgerKingAppApp.swift
+//  BurgerKingApp
+//
+//  Created by cmStudent on 2024/10/13.
+//
+
+import SwiftUI
+
+@main
+struct BurgerKingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
