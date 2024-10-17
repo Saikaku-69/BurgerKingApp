863fb6475110c163caeac4b177462246ad899400
Saikaku <grnmnsta@gmail.com>
2024-10-17 11:11:44 +0900

    probability fix

--- BurgerKingApp/Feature/GameView.swift
+++ BurgerKingApp/Feature/GameView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import UIKit
 
 //Burger構造体
 struct Burger:Identifiable {
@@ -26,54 +27,57 @@ struct GameView: View {
     @State private var score:Int = 0
     @State private var gameTimeCount: Double = 60
     @State private var getScore:Int = 1
+    //確率
     @State private var grafUpProbability:Int = 5
-    @State private var goldBurgerProbability:Int = 10
-    @State private var compassProbability:Int = 15
-    @State private var vagetableProbability:Int = 20
-    
+    @State private var goldBurgerProbability:Int = 5
+    @State private var compassProbability:Int = 5
+    @State private var vagetableProbability:Int = 5
+    //プレイ画面
     @State private var gameScreenWidth:CGFloat = UIScreen.main.bounds.width-50
     @State private var gameScreenHeight:CGFloat = UIScreen.main.bounds.height-200
-    
-    //Player Position
+    //Playerデータ
     @State private var playerPositionX:CGSize = .zero
     @GestureState private var dragPositionX:CGSize = .zero
     @State private var playerPositionY:CGFloat = UIScreen.main.bounds.height-200
     @State private var playerFrame:CGFloat = 80
-    @State private var playerRotation:Double = 0
     @State private var playerOpacity:Double = 1.0
-    @State private var playerAction:Bool = true
-    //Create burger
-    @State private var GetBurger:[Burger] = []
-    @State private var GetPoo:[Poo] = []
-    //collision mainObject Size
+    @State private var playerDisable:Bool = true
+    //当たる判定のSize
     @State private var mainObWidth:CGFloat = 30
     @State private var mainObHeight:CGFloat = 5
-    
-    //
-    @State private var saveTimer: Timer?
+    //生成する構造体
+    @State private var GetBurger:[Burger] = []
+    @State private var GetPoo:[Poo] = []
+    //タイマー
+    @State private var countTimer: Timer?
     @State private var fallingTimer: Timer?
     @State private var createBurgerTimer: Timer?
     @State private var pooTimer: Timer?
+    //落ちるスピード&生成スピード
     @State private var fallingSpeed:Double = 0.005
     @State private var createSpeed:Double = 0.3
     //ペナルティ階段
     @State private var hStackCount:CGFloat = 0
     @State private var showPoo: Bool = false
     @State private var deadLine:CGFloat = UIScreen.main.bounds.height - 210
-    //
+    //ゲーム開始&終了ボタン
     @State private var gameStartButton:Bool = true
     @State private var gameOver:Bool = false
+    @State private var scoreColor:Bool = false
     var body: some View {
         ZStack {
             Color.backgroundColor.edgesIgnoringSafeArea(.all)
             
             VStack {
-                //ゲーム時間を表す
                 HStack {
                     Image("compass")
                         .resizable()
                         .frame(width: 30,height: 30)
-                    Text("GAME TIME:\(Int(gameTimeCount))")
+                    Text("GAME TIME: ")
+                        .foregroundColor(.white)
+                        .fontWeight(.bold)
+                    Text("\(Int(gameTimeCount))")
+                        .font(.system(size: 50))
                         .foregroundColor(.white)
                         .fontWeight(.bold)
                 }
@@ -83,7 +87,7 @@ struct GameView: View {
                 ZStack {
                     //Play画面
                     VStack {
-                        Text("BEST SCORE:\(Int(bestScore))")
+                        Text("BEST SCORE: \(Int(bestScore))")
                             .foregroundColor(.white)
                             .fontWeight(.bold)
                             .opacity(0.5)
@@ -94,25 +98,22 @@ struct GameView: View {
                                 .frame(width:30)
                             Text("X \(Int(score))")
                         }
-                        .foregroundColor(.white)
+                        .foregroundColor(scoreColor ? Color.red : Color.white)
                         .fontWeight(.bold)
                         .opacity(0.9)
                         Spacer()
                         //Play画面の高さ
                         Rectangle()
                             .fill(Color.backgroundColor)
-                            .frame(width: gameScreenWidth,
-                                   height:Burger.burgerHeight * hStackCount)
+                            .frame(width: gameScreenWidth,height:Burger.burgerHeight * hStackCount)
                     }
                     .frame(width: gameScreenWidth,height:gameScreenHeight)
-                    
                     //PlayerImage
                     Image("ManDefault")
                         .resizable()
                         .aspectRatio(contentMode: .fit)
                         .frame(height:playerFrame)
-                        .position(x:playerPositionX.width + dragPositionX.width + 10,
-                                  y:playerPositionY - 40)
+                        .position(x:playerPositionX.width + dragPositionX.width + 10,y:playerPositionY - 40)
                         .opacity(playerOpacity)
                         .gesture(
                             DragGesture()
@@ -123,8 +124,7 @@ struct GameView: View {
                                     playerPositionX.width += value.translation.width
                                 }
                         )
-                        .disabled(playerAction)
-                    
+                        .disabled(playerDisable)
                     //当たる判定用mainObject
                     Rectangle()
                         .fill(.red)
@@ -132,7 +132,6 @@ struct GameView: View {
                         .position(x:playerPositionX.width + dragPositionX.width,
                                   y:playerPositionY - 60)
                         .opacity(0.0)
-                    
                     ForEach(GetBurger) { item in
                         Image(item.imageName)
                         .resizable()
@@ -140,7 +139,6 @@ struct GameView: View {
                         .frame(width:Burger.burgerWidth)
                         .position(item.burgerPosition)
                     }
-                    
                     ForEach(GetPoo) { poo in
                         Image("poo")
                         .resizable()
@@ -150,8 +148,7 @@ struct GameView: View {
                     }
                     
                 }
-                .frame(width: gameScreenWidth,
-                       height: gameScreenHeight)
+                .frame(width: gameScreenWidth,height: gameScreenHeight)
                 .background(Color.gameBackgroundColor)
                 
                 ZStack {
@@ -174,7 +171,7 @@ struct GameView: View {
                                     playerPositionX.width += value.translation.width
                                 }
                         )
-                        .disabled(playerAction)
+                        .disabled(playerDisable)
                 }
                 .frame(width: gameScreenWidth,height: 50)
             }
@@ -195,10 +192,6 @@ struct GameView: View {
             if gameOver {
                 VStack {
                     Image("french")
-                    //ranking
-//                    Text("")
-//                    Text("")
-//                    Text("")
                     Button(action: {
                         initialGame()
                     }) {
@@ -222,38 +215,35 @@ struct GameView: View {
         .frame(maxWidth: .infinity, maxHeight: .infinity)
         .onAppear() {
             playerPositionX.width = UIScreen.main.bounds.width/2 - 30
-            bestScoreCalculate()
         }
         .onDisappear() {
-            saveTimer?.invalidate()
+            countTimer?.invalidate()
             createBurgerTimer?.invalidate()
             fallingTimer?.invalidate()
+            pooTimer?.invalidate()
         }
     }
     //Burger構造体
     private func createBurger() {
         let randomNumber = Int.random(in: 1...100)
         let randomImage: String
-        //regular = 80 ,score = 5,time = 5,penalty = 5, double
         if randomNumber <= grafUpProbability  {
             randomImage = "grafup"
-        } else if randomNumber <= goldBurgerProbability {
+        } else if randomNumber <= grafUpProbability + goldBurgerProbability {
             randomImage = "BurgerBreak"
-        } else if randomNumber <= compassProbability {
+        } else if randomNumber <= grafUpProbability + goldBurgerProbability + compassProbability {
             randomImage = "compass"
-        } else if randomNumber <= vagetableProbability {
+        } else if randomNumber <= grafUpProbability + goldBurgerProbability + compassProbability + vagetableProbability {
             randomImage = "vagetable"
         } else {
             randomImage = "Burger"
         }
-        
         let randomX = CGFloat.random(in: Burger.burgerWidth/2 + 30...(gameScreenWidth-40))
         let newItem = Burger(imageName: randomImage, burgerPosition:CGPoint(x:randomX,y:10))
         GetBurger.append(newItem)
     }
     private func createPoo() {
-        let newPoo = Poo(pooPosition:CGPoint(x:playerPositionX.width + dragPositionX.width + 10,
-                                             y:playerPositionY))
+        let newPoo = Poo(pooPosition:CGPoint(x:playerPositionX.width + dragPositionX.width + 10,y:playerPositionY))
         GetPoo.append(newPoo)
     }
     private func pooAction() {
@@ -268,7 +258,6 @@ struct GameView: View {
             }
         }
     }
-    //Falling
     private func falling() {
         fallingTimer?.invalidate()
         fallingTimer = Timer.scheduledTimer(withTimeInterval: fallingSpeed, repeats: true) { _ in
@@ -280,7 +269,7 @@ struct GameView: View {
                     }
                 } else if itemName.imageName == "Burger" {
                     GetBurger.remove(at: index)
-                    if hStackCount <= 7 {
+                    if hStackCount <= 8 {
                         hStackCount += 1
                         playerPositionY -= Burger.burgerHeight
                         deadLine -= Burger.burgerHeight
@@ -294,9 +283,9 @@ struct GameView: View {
         }
     }
     private func startGame() {
-        playerAction = false
-        saveTimer?.invalidate()
-        countSaveTime()
+        playerDisable = false
+        countTimer?.invalidate()
+        countGameTime()
         createBurgerTimer = Timer.scheduledTimer(withTimeInterval: createSpeed, repeats: true) { _ in
             createBurger()
             falling()
@@ -304,43 +293,40 @@ struct GameView: View {
     }
     private func stopGame() {
         if gameTimeCount <= 0 {
-            saveTimer?.invalidate()
-            createBurgerTimer?.invalidate()
-            fallingTimer?.invalidate()
-            fallingSpeed = 0.005
-            playerAction = true
-            playerOpacity = 0.0
-            bestScoreCalculate()
+            stopAllTimer()
+            playerDisable = true
             GetBurger.removeAll()
             GetPoo.removeAll()
+            bestScoreCalculate()
             deadLine = UIScreen.main.bounds.height-200
+            fallingSpeed = 0.005
             DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
                 hStackCount = 0
                 gameOver = true
             }
         }
     }
+    private func stopAllTimer() {
+        countTimer?.invalidate()
+        createBurgerTimer?.invalidate()
+        fallingTimer?.invalidate()
+        pooTimer?.invalidate()
+    }
     private func initialGame() {
         gameOver = false
         gameStartButton = true
         score = 0
         gameTimeCount = 60
-        playerFrame = 80
-        playerOpacity = 1.0
         playerPositionY = UIScreen.main.bounds.height-200
         playerPositionX.width = UIScreen.main.bounds.width/2 - 30
-        deadLine = UIScreen.main.bounds.height-200
+        playerOpacity = 1.0
     }
     //判定
     private func collision() {
-        //main
-        let newMainPosition = CGPoint(x:playerPositionX.width + dragPositionX.width,
-                                        y:playerPositionY - 50)
-        let newMainFrame = CGRect(x:newMainPosition.x - mainObWidth/1.5,
-                                  y:newMainPosition.y,
-                                  width:mainObWidth,
-                                  height:mainObHeight)
-        //配列中のBurgerを判定しら削除
+        let newMainPosition = CGPoint(x:playerPositionX.width + dragPositionX.width,y:playerPositionY - 50)
+        let newMainFrame = CGRect(x:newMainPosition.x - mainObWidth/1.5,y:newMainPosition.y,
+                                  width:mainObWidth,height:mainObHeight)
+        //配列中のBurgerを判定した後削除
         for index in GetBurger.indices.reversed() {
             let newBurgerRect = CGRect(x: GetBurger[index].burgerPosition.x - Burger.burgerWidth / 2,
                                        y: GetBurger[index].burgerPosition.y - Burger.burgerHeight / 2,
@@ -350,40 +336,43 @@ struct GameView: View {
                 let itemName = GetBurger[index]
                 if itemName.imageName == "Burger" {
                     generateImpactFeedback(for: .medium)
-                    score += getScore
                     GetBurger.remove(at: index)
+                    score += getScore
                     createPoo()
                     pooAction()
                 } else if itemName.imageName == "compass" {
                     generateImpactFeedback(for: .heavy)
-                    gameTimeCount += 5
                     GetBurger.remove(at: index)
+                    gameTimeCount += 5
                 } else if itemName.imageName == "BurgerBreak" {
                     generateImpactFeedback(for: .heavy)
+                    GetBurger.remove(at: index)
+                    GetBurger.removeAll()
                     grafUpProbability = 0
                     goldBurgerProbability = 0
                     compassProbability = 0
                     vagetableProbability = 0
-                    GetBurger.remove(at: index)
                     DispatchQueue.main.asyncAfter(deadline: .now() + 5) {
                         grafUpProbability = 5
-                        goldBurgerProbability = 10
-                        compassProbability = 15
-                        vagetableProbability = 20
+                        goldBurgerProbability = 5
+                        compassProbability = 5
+                        vagetableProbability = 5
                     }
                 } else if itemName.imageName == "grafup" {
                     generateImpactFeedback(for: .heavy)
-                    getScore = 5
-                    compassProbability = 0
                     GetBurger.remove(at: index)
+                    scoreColor = true
+                    getScore = 5
+                    grafUpProbability = 0
                     DispatchQueue.main.asyncAfter(deadline: .now() + 5) {
+                        scoreColor = false
                         getScore = 1
-                        compassProbability = 5
+                        grafUpProbability = 5
                     }
                 } else if itemName.imageName == "vagetable" {
                     generateErrorFeedback()
-                    showPoo = true
                     GetBurger.remove(at: index)
+                    showPoo = true
                     DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
                         showPoo = false
                     }
@@ -405,17 +394,17 @@ struct GameView: View {
         //分别对应通知,错误和警告
         feedbackGenerator.notificationOccurred(.error)
     }
-    private func countSaveTime() {
-        saveTimer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { _ in
+    private func countGameTime() {
+        countTimer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { _ in
             gameTimeCount -= 1
             speedChange()
         }
     }
     private func speedChange() {
-        if fallingSpeed >= 0.001 {
-            fallingSpeed -= 0.004 / 60
+        if fallingSpeed >= 0.002 {
+            fallingSpeed -= 0.003 / 60
         } else  {
-            fallingSpeed = 0.001
+            fallingSpeed = 0.002
         }
     }
     private func bestScoreCalculate() {
