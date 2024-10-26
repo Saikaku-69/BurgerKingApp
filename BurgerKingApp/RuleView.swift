4d481472ecd3ad68aeeaaa1bb55792ba3d4aa715
Saikaku <grnmnsta@gmail.com>
2024-10-26 19:07:52 +0900

    rule changed

--- BurgerKingApp/RuleView.swift
+++ BurgerKingApp/RuleView.swift
@@ -18,9 +18,10 @@ let items = [
     Items(img: "Burger", imgName: "バーガー", explanation: "消化量+1"),
     Items(img: "GoldBurger", imgName: "ゴールドバーガー", explanation: "消化量+20"),
     Items(img: "grafup", imgName: "ボーナスタイム", explanation: "消化量2倍"),
-    Items(img: "clock", imgName: "時計", explanation: "運試し"),
+    Items(img: "clock", imgName: "時計", explanation: "時間+3秒"),
     Items(img: "hammer", imgName: "ハンマー", explanation: "土台を一階壊す"),
-    Items(img: "vagetable", imgName: "幽霊", explanation: "邪魔する"),
+    Items(img: "hatena", imgName: "運試し", explanation: "ボーナスOR加速"),
+    Items(img: "vagetable", imgName: "幽霊", explanation: "邪魔者が出る")
 ]
 
 struct RuleView: View {
