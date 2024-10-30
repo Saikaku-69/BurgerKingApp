96b20fe5e9f8ea89354bcc4f13a9052f0e234be7
Saikaku <grnmnsta@gmail.com>
2024-10-30 15:44:08 +0900

    v2.1.1: Add Opening view

---
+++ BurgerKingApp/v2.1.0/TitleRankView.swift
@@ -0,0 +1,18 @@
+//
+//  TitleRankView.swift
+//  BurgerKingApp
+//
+//  Created by cmStudent on 2024/10/30.
+//
+
+import SwiftUI
+
+struct TitleRankView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TitleRankView()
+}
